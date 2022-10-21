c93f1afce49ef9a196e46c0234a4e1a8cb282a84
AkashNarale <akashnarale43@gmail.com>
2022-10-21 12:41:08 +0530

    UC4

---
+++ RegexUC4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19_Regex
+{
+    internal class RegexUC4
+    {
+        public static void UC4()
+        {
+            Console.WriteLine("\nMobile No :");
+            string inStr = Console.ReadLine();
+            string Mobile = "^[0-9]{2}[ ]*[0-9]{10}$";
+
+            Regex regex = new Regex(Mobile);
+            if (regex.IsMatch(inStr))
+            {
+                Console.WriteLine("\nValid");
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid");
+            }
+        }
+    }
+}
